fc1350878533380a19d61eda378fb34ebad58e7c
Arthur Basso <arthurkbasso@gmail.com>
2025-07-08 14:26:16 -0300

    teste homolog+prod

--- migrations.sql
+++ migrations.sql
@@ -1,4 +1,8 @@
-CREATE TABLE IF NOT EXISTS nova_tabela_teste4 (
+DROP TABLE IF EXISTS nova_tabela_teste2;
+DROP TABLE IF EXISTS nova_tabela_teste3;
+DROP TABLE IF EXISTS nova_tabela_teste4;
+
+CREATE TABLE IF NOT EXISTS nova_tabela_teste (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL
 );
