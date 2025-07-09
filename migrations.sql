a3b57c99fd4f3281e04af48f90604f81fb3b4cad
Arthur Basso <arthurkbasso@gmail.com>
2025-07-09 11:32:44 -0300

    test 15

--- migrations.sql
+++ migrations.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS nova_tabela_teste2;
+DROP TABLE IF EXISTS nova_tabela_teste3;
 
-CREATE TABLE IF NOT EXISTS nova_tabela_teste3 (
+CREATE TABLE IF NOT EXISTS teste (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL
 );
